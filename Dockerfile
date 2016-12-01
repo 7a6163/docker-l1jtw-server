c6da87b6fd82a6e9e5e9906ee37204c2d4b7c280
Zac <mymengyu@gmail.com>
2016-12-01 20:12:01 +0800

    Fixed Rate Properties

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,5 @@ MAINTAINER Zac
 RUN apk add --no-cache bash mysql-client unzip
 
 WORKDIR /data
-COPY L1J-TW_3.80c /data
 
 CMD sh /data/ServerStart.sh
--- L1J-TW_3.80c/ServerStart.sh
+++ L1J-TW_3.80c/ServerStart.sh
@@ -14,20 +14,20 @@ sed 's/mysql:\/\/localhost/mysql:\/\/'"$DB_HOST"'/g' server.properties > custom.
 sed 's/Password=password/Password='"$DB_PWD"'/g' server.properties > custom.server; cat custom.server > server.properties
 
 # Exp Rate
-sed 's/RateXp = 1.0/RateXp = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
-sed 's/RateLawful = 1.0/RateLawful = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
-sed 's/RateKarma = 1.0/RateKarma = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
-sed 's/RateDropAdena = 1.0/RateDropAdena = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
-sed 's/RateDropItems = 1.0/RateDropItems = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
+# sed 's/RateXp = 1.0/RateXp = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
+# sed 's/RateLawful = 1.0/RateLawful = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
+# sed 's/RateKarma = 1.0/RateKarma = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
+# sed 's/RateDropAdena = 1.0/RateDropAdena = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
+# sed 's/RateDropItems = 1.0/RateDropItems = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 
 # Enchant Chance
-sed 's/EnchantChanceWeapon = 0/EnchantChanceWeapon = '"$ENCHANT_CHANCE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
-sed 's/EnchantChanceArmor = 0/EnchantChanceArmor = '"$ENCHANT_CHANCE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
-sed 's/AttrEnchantChance = 10/AttrEnchantChance = '"$ENCHANT_CHANCE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
+# sed 's/EnchantChanceWeapon = 0/EnchantChanceWeapon = '"$ENCHANT_CHANCE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
+# sed 's/EnchantChanceArmor = 0/EnchantChanceArmor = '"$ENCHANT_CHANCE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
+# sed 's/AttrEnchantChance = 10/AttrEnchantChance = '"$ENCHANT_CHANCE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 
 # Unzip Maps
 unzip /data/maps/352_maps.zip -d /data/maps/
 
 cd /data
 
-java -Xmx512m -Xincgc -jar l1jserver.jar
+java -Xmx2048m -Xincgc -jar l1jserver.jar
